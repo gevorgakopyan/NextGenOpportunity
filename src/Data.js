e046e496f73db48f7c14bee591a5ce1f6fb39be2
gevorgakopyan <gevorgakopyan01@gmail.com>
2023-12-04 15:54:30 -0800

    full data page

--- src/Data.js
+++ src/Data.js
@@ -3,6 +3,8 @@ import Navbar from './Navbar';
 import Link from '@mui/material/Link';
 import TableauChart from './TableauChart';
 import { Box, Container, Typography, Button} from '@mui/material';
+import datapic from './img/datapic.jpg'
+import diversityKidsPic from './img/diversitykids.jpg'
 
 const Data = () => {
   return (
@@ -10,10 +12,10 @@ const Data = () => {
       <Navbar />
       <Container mt={0} sx={{ paddingBottom: '2rem' }}>
         <Box textAlign={'center'}>
-          <Typography variant='h4' sx={{color: 'gray'}} mt={3}>
+          <Typography variant='h4' sx={{color: '#046c75'}} mt={3}>
             Data
           </Typography>
-          <Typography variant='h5' sx={{color: 'gray'}} mt={3}>
+          <Typography variant='h5' sx={{color: '#046c75'}} mt={3}>
             Our Dataset: The Child Opportunity Index
           </Typography>
           <Link href="https://data.diversitydatakids.org/dataset/coi20_zipcodes-child-opportunity-index-2-0-zip-code-data" underline="none">
@@ -21,7 +23,7 @@ const Data = () => {
                 variant="contained"
                 sx={{
                     marginTop: '2rem',
-                    backgroundColor: 'secondary.main',
+                    backgroundColor: '#046c75',
                     color: 'white',
                     '&:hover': {
                         backgroundColor: 'primary.dark',
@@ -33,14 +35,14 @@ const Data = () => {
                     Explore The Complete Dataset
                 </Button>
           </Link>
-          <Typography variant='h5' sx={{color: 'gray'}} mt={3}>
+          <Typography variant='h5' sx={{color: '#046c75'}} mt={3}>
             About The Dataset 
           </Typography>
           <Typography
             variant='body1'
             sx={{
               margin: '0.5rem auto',
-              maxWidth: 'calc(min(90%, 550px))',
+              maxWidth: 'calc(min(90%, 850px))',
               textAlign: 'justify',
               color: 'black'
             }}
@@ -51,7 +53,7 @@ const Data = () => {
             variant='body1'
             sx={{
               margin: '0.5rem auto',
-              maxWidth: 'calc(min(90%, 550px))',
+              maxWidth: 'calc(min(90%, 850px))',
               textAlign: 'justify',
               color: 'black'
             }}
@@ -62,21 +64,32 @@ const Data = () => {
             variant='body1'
             sx={{
               margin: '0.5rem auto',
-              maxWidth: 'calc(min(90%, 550px))',
+              maxWidth: 'calc(min(90%, 850px))',
               textAlign: 'justify',
               color: 'black'
             }}
           >
             For each zip code, the dataset lists the population between 0 and 17, the estimated number of each demographic group, the weighted averages/standard deviations of the scores for each of the domains and the overall child opportunity levels, and a categorical measure of each domain as “Very High”, “High”,  “Moderate”, “Low,” or “Very Low”. The values of each of the domains and the overall child opportunity levels were standardized nationally, by state, and by metro so the researchers can examine these measures in regard to a more localized context or on a broader scale. This data may reveal differences in opportunities for children living across the United States and how a child’s zip code may determine a child’s future successes and challenges.
           </Typography>
-          <Typography variant='h5' sx={{color: 'gray'}} mt={3}>
+          <Typography variant='h5' sx={{color: '#046c75'}} mt={3}>
             Who Created This Data?
           </Typography>
+          <Box component="img" 
+                 src= {diversityKidsPic} // Replace with your image path
+                 alt="Diversity Data"
+                 sx={{
+                    maxWidth: '50%',
+                    alignItems:'left',
+                    height: '50%',
+                    marginTop: '1rem',
+                    marginBottom: '1rem'
+                 }}
+            />
           <Typography
             variant='body1'
             sx={{
               margin: '0.5rem auto',
-              maxWidth: 'calc(min(90%, 550px))',
+              maxWidth: 'calc(min(90%, 850px))',
               textAlign: 'justify',
               color: 'black'
             }}
@@ -87,21 +100,21 @@ const Data = () => {
             variant='body1'
             sx={{
               margin: '0.5rem auto',
-              maxWidth: 'calc(min(90%, 550px))',
+              maxWidth: 'calc(min(90%, 850px))',
               textAlign: 'justify',
               color: 'black'
             }}
           >
             The organization behind the Child Opportunity Index, Diversity Data Kids, acknowledges on its website that it receives funding from the W.K. Kellogg Foundation and the Robert Wood Johnson Foundation. The W.K. Kellogg Foundation is a private foundation with the mission to support equity in opportunity for disadvantaged children. The Robert Wood Johnson Foundation, also a private foundation, is dedicated to advancing health equity. The mission of both foundations are aligned with the aim of Diversity Data Kids in creating the Child Opportunity Index. As stated on their webpage, the COI was created to “widen the national conversation about addressing inequality to include not only income and wealth but also the neighborhood environments that our children experience.” As such, it appears that equity is the unifying theme for both foundations and the organization. This search for equity may have driven the data collection process and the research done on the dataset. No other funding sources have been identified outside of these two.
           </Typography>
-          <Typography variant='h5' sx={{color: 'gray'}} mt={3}>
+          <Typography variant='h5' sx={{color: '#046c75'}} mt={3}>
             Who Information is Left Out?
           </Typography>
           <Typography
             variant='body1'
             sx={{
               margin: '0.5rem auto',
-              maxWidth: 'calc(min(90%, 550px))',
+              maxWidth: 'calc(min(90%, 850px))',
               textAlign: 'justify',
               color: 'black'
             }}
@@ -112,7 +125,7 @@ const Data = () => {
             variant='body1'
             sx={{
               margin: '0.5rem auto',
-              maxWidth: 'calc(min(90%, 550px))',
+              maxWidth: 'calc(min(90%, 850px))',
               textAlign: 'justify',
               color: 'black'
             }}
@@ -123,35 +136,46 @@ const Data = () => {
             variant='body1'
             sx={{
               margin: '0.5rem auto',
-              maxWidth: 'calc(min(90%, 550px))',
+              maxWidth: 'calc(min(90%, 850px))',
               textAlign: 'justify',
               color: 'black'
             }}
           >
             Also, even in the same region, there can be sufficient socioeconomic differences within the region. For example, even in the same state of California, there are stark differences between cities such as tech-driven San Francisco and agricultural Bakersfield. Furthermore, there are differences between neighborhoods within cities, like between the upscale Beverly Hills and the lower income South Central area, both within Los Angeles. 
           </Typography>
-          <Typography variant='h5' sx={{color: 'gray'}} mt={3}>
+          <Typography variant='h5' sx={{color: '#046c75'}} mt={3}>
             Separating Zip Codes into High and Low Population
           </Typography>
           <Typography
             variant='body1'
             sx={{
               margin: '0.5rem auto',
-              maxWidth: 'calc(min(90%, 550px))',
+              maxWidth: 'calc(min(90%, 850px))',
               textAlign: 'justify',
               color: 'black'
             }}
           >
             Because we were interested exploring differences between low and high populated areas in the United States, we created a new categorical variable, high_low_pop, that designated each zip code as low or high population, depending on whether its population, indicated by the existing pop variable, fell lower or higher than the median value of the pop variable. This newly created variable was helpful in comparing education levels across high or low populated zip codes. 
           </Typography>
-          <Typography variant='h5' sx={{color: 'gray'}} mt={3}>
+          <Typography variant='h5' sx={{color: '#046c75'}} mt={3}>
             Focus On Education
           </Typography>
+          <Box component="img" 
+                 src= {datapic} // Replace with your image path
+                 alt="Diversity Data"
+                 sx={{
+                    maxWidth: '90%',
+                    alignItems:'left',
+                    height: 'auto',
+                    marginTop: '1rem',
+                    marginBottom: '1rem'
+                 }}
+            />
           <Typography
             variant='body1'
             sx={{
               margin: '0.5rem auto',
-              maxWidth: 'calc(min(90%, 550px))',
+              maxWidth: 'calc(min(90%, 850px))',
               textAlign: 'justify',
               color: 'black'
             }}
@@ -162,19 +186,19 @@ const Data = () => {
             variant='body1'
             sx={{
               margin: '0.5rem auto',
-              maxWidth: 'calc(min(90%, 550px))',
+              maxWidth: 'calc(min(90%, 850px))',
               textAlign: 'justify',
               color: 'black'
             }}
           >
             Though our dataset focuses on child opportunity as a whole, we decided to make the focal point of our exploration child education because high quality education is strongly linked to social mobility and healthy child development. In America, people view education as intrinsically connected to the American dream, with hard work and a good education leading to a better life. However, the quality of education across our country is not equal. As you can see from the following visualization of our dataset, child education levels vary greatly in the United States.
           </Typography>
-          <TableauChart url={"https://public.tableau.com/shared/D2SXNY49M?:display_count=n&:origin=viz_share_link"} options={{hideTabs: false}}/>
+          <TableauChart url={"https://public.tableau.com/views/Datavizproject_17011575078090/MAP?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link ic.tableau.com/shared/D2SXNY49M?:display_count=n&:origin=viz_share_link"} options={{hideTabs: false}}/>
           <Typography
             variant='body1'
             sx={{
               margin: '0.5rem auto',
-              maxWidth: 'calc(min(90%, 550px))',
+              maxWidth: 'calc(min(90%, 850px))',
               textAlign: 'justify',
               color: 'black'
             }}
